67947ae49dabd96dd373bf603f0aa1f412c95b62
SSL <devnull@localhost>
2016-01-15 16:42:21 +0100

    Added NHibernate map + NHibernate dependency.

---
+++ src/UCommerce.Transactions.Payments.Giftcard/Services/GiftCardCodeGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UCommerce.Marketing.Targets
+{
+    public class GiftCardCodeGenerator : IVoucherCodeGenerator
+    {
+        public IList<string> GenerateCodes(int numberToGen, int lengthOfVoucher, string prefix, string suffix)
+        {
+            var codes = new List<string>();
+
+            string seperatorPrefix = "", seperatorSuffix = "";
+            if (!string.IsNullOrEmpty(prefix))
+                seperatorPrefix = "-";
+
+            if (!string.IsNullOrEmpty(suffix))
+                seperatorSuffix = "-";
+
+
+
+            return codes;
+        }
+    }
+}
