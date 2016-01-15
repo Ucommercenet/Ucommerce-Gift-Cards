29d2da9c19d4435834881f615572377589b36bc8
Mads Due <devnull@localhost>
2016-01-15 17:06:51 +0100

    Added map for the new Role.

---
+++ src/UCommerce.Transactions.Payments.Giftcard/Entities/Maps/CreateGiftCardRoleMap.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Mapping;
+using UCommerce.Transactions.Payments.Giftcard.Entities.Security;
+
+namespace UCommerce.Transactions.Payments.Giftcard.Entities.Maps
+{
+    class CreateGiftCardRoleMap : SubclassMap<CreateGiftCardRole>
+    {
+        public CreateGiftCardRoleMap()
+        {
+            Table("uCommerce_Role");
+            DiscriminatorValue(170);
+        }
+    }
+}
