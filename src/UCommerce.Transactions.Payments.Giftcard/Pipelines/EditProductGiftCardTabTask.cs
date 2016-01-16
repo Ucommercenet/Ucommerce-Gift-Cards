7a684b467cb566983c979c9ff86d214b27c9c5c1
Mads Due <devnull@localhost>
2016-01-16 20:50:39 +0100

    Add and remove tabs to edit product when product definition is Gift Card.

--- src/UCommerce.Transactions.Payments.Giftcard/Pipelines/EditProductGiftCardTabTask.cs
+++ src/UCommerce.Transactions.Payments.Giftcard/Pipelines/EditProductGiftCardTabTask.cs
@@ -1,34 +1,47 @@
-﻿using UCommerce.Pipelines;
+﻿using UCommerce.EntitiesV2;
+using UCommerce.Pipelines;
 using UCommerce.Presentation.UI;
+using UCommerce.Presentation.Web;
 
 namespace UCommerce.Transactions.Payments.GiftCard.Pipelines
 {
-	public class EditProductGiftCardTabTask : IPipelineTask<SectionGroup>
-	{
-		public PipelineExecutionResult Execute(SectionGroup sectionGroup)
-		{
-			if (sectionGroup.GetViewName() != "editproduct_aspx") return PipelineExecutionResult.Success;
+    public class EditProductGiftCardTabTask : IPipelineTask<SectionGroup>
+    {
+        private readonly IRepository<Product> _productRepository;
 
-            var section = BuildSection(sectionGroup);
-           
-			sectionGroup.Sections.Insert(1, section);
-            sectionGroup.Controls.Add(section);
+        public EditProductGiftCardTabTask(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
 
-			return PipelineExecutionResult.Success;
-		}
+        public PipelineExecutionResult Execute(SectionGroup sectionGroup)
+        {
+            if (sectionGroup.GetViewName() != "editproduct_aspx") return PipelineExecutionResult.Success;
 
-		private Section BuildSection(SectionGroup sectionGroup)
-		{
-			var section = new Section
-			{
-				Name = "Gift Card Price",
-				ID = sectionGroup.CreateUniqueControlId()
-			};
+            var product = _productRepository.Get(QueryString.Common.Id);
 
-			var control = sectionGroup.View.LoadControl("/Apps/UCommerce.GiftCards/EditGiftCardPrices.ascx");
+            if (product.ProductDefinition.Name == Constants.GiftCardProductDefinition)
+            {
+                var section = BuildSection(sectionGroup);
+                sectionGroup.Sections.Insert(1, section);
+                sectionGroup.Controls.Add(section);
+            }
 
-			section.AddControl(control);
-			return section;
-		}
-	}
+            return PipelineExecutionResult.Success;
+        }
+
+        private Section BuildSection(SectionGroup sectionGroup)
+        {
+            var section = new Section
+            {
+                Name = "Gift Card Price",
+                ID = sectionGroup.CreateUniqueControlId()
+            };
+
+            var control = sectionGroup.View.LoadControl("/Apps/UCommerce.GiftCards/EditGiftCardPrices.ascx");
+
+            section.AddControl(control);
+            return section;
+        }
+    }
 }
--- src/UCommerce.Transactions.Payments.Giftcard/Pipelines/RemovePriceAndVariantsTabsFromEditProductTask.cs
+++ src/UCommerce.Transactions.Payments.Giftcard/Pipelines/RemovePriceAndVariantsTabsFromEditProductTask.cs
@@ -1,14 +1,29 @@
 ﻿using System.Linq;
+using UCommerce.EntitiesV2;
 using UCommerce.Pipelines;
 using UCommerce.Presentation.UI;
+using UCommerce.Presentation.Web;
 
 namespace UCommerce.Transactions.Payments.GiftCard.Pipelines
 {
     public class RemovePriceAndVariantsTabsFromEditProductTask : IPipelineTask<SectionGroup>
     {
+        private readonly IRepository<Product> _productRepository;
+
+        public RemovePriceAndVariantsTabsFromEditProductTask(IRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
         public PipelineExecutionResult Execute(SectionGroup sectionGroup)
         {
             if (sectionGroup.GetViewName() != "editproduct_aspx") return PipelineExecutionResult.Success;
+
+            var product = _productRepository.Get(QueryString.Common.Id);
+
+            if (product.ProductDefinition.Name != Constants.GiftCardProductDefinition) { 
+                return PipelineExecutionResult.Success;
+            }
 
             var tabsToRemove = sectionGroup.Sections.Where(x => x.OriginalName == UCommerce.Constants.UI.Sections.Stores.Product.Pricing || x.OriginalName == UCommerce.Constants.UI.Sections.Stores.Product.Variants).ToList();
 
