b61de07255747620a0375d417312f5b3b294d747
Thomas Arvidsen <devnull@localhost>
2016-01-16 15:01:32 +0100

    Removed unused imports.

--- src/UCommerce.Transactions.Payments.Giftcard/Entities/Maps/MapAssemblyTag.cs
+++ src/UCommerce.Transactions.Payments.Giftcard/Entities/Maps/MapAssemblyTag.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UCommerce.EntitiesV2;
+﻿using UCommerce.EntitiesV2;
 
 namespace UCommerce.Transactions.Payments.GiftCard.Entities.Maps
 {
